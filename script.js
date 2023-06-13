df82710982d96c051fa1b0d3ed23b6d51f83479a
Hernan Polo <hernan.polo1985@gmail.com>
2023-06-13 14:14:15 -0300

    adicionando condicional e alert

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 alert("exercicio Git Github")
 var nome = prompt("Digite seu nome")
-alert(nome+", seja bem-vindo!")
+var idadeString = prompt("Digite sua idade")
+var idade = Number(idadeString)
+if (idade >= 18){
+    alert(nome+", seja bem-vindo!") 
+} else {
+    alert(nome+",infelizmente ainda não tem idade para ingresar")
+}
